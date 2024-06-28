5e53eeef6fa48db3aa4086fda28f74de800ef923
Adebola <adebola@poptin.com>
2024-06-28 17:05:00 +0100

    NavBar component design

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/products">Products</router-link>
-    </nav>
+    <NavBar />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 * {
